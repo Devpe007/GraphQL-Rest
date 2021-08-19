84a015703b7f9125253e69bd55380b342f17a821
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-08-19 20:29:59 -0300

    created GetPostByUserController

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
 import { CreateUserController } from '@modules/users/controllers/CreateUserController';
+import { GetPostsByUserController } from '@modules/posts/controllers/GetPostsByUserController';
 
 const createUserController = new CreateUserController();
+const getPostsByUserController = new GetPostsByUserController();
 
 const routes = Router();
 
 routes.post('/users', createUserController.handle);
+
+routes.get('/posts/user/:id', getPostsByUserController.handle);
 
 export { routes };
