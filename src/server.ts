51d01d9b4c3117ca5c65f8a82f0f156648d49dfe
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-08-19 16:27:04 -0300

    created the error handling file

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import mongoose from "mongoose";
 
+import { middlewareError } from './middlewares/error/Error';
+
 mongoose.connect("mongodb://localhost:27017/code_drops", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -12,14 +14,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof Error) {
-      return response.status(400).json(error.message);
-    }
-
-    return response.status(500).json(error);
-  }
-);
+app.use(middlewareError);
 
 app.listen(4003, () => console.log("Server is running"));
